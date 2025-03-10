3fa050fec568054f3f8ed3bccd08651f2db4e784
HassanAsrhaf <muhammad.hassan@futuredevsolutions.com>
2025-03-10 10:57:24 +0500

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,14 @@ const authUser = async (req, res, next) => {
 
     const { token } = req.headers;
 
-    if (!token) {
-        return res.json({ success: false, message: 'Not Authorized Login Again' })
-    }
     // if (!token) {
-    //     // Allow guest users to proceed without setting `userId`
-    //     req.body.userId = null;
-    //     return next();  // Continue to the next middleware
+    //     return res.json({ success: false, message: 'Not Authorized Login Again' })
     // }
+    if (!token) {
+        // Allow guest users to proceed without setting `userId`
+        req.body.userId = null;
+        return next();  // Continue to the next middleware
+    }
     try {
 
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
